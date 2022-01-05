d8243b99835e23ee4c6d698909752bd737ea2b56
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-05 14:09:39 +0000

    exported and imported bag.js to passenger.js

--- day 2/bag.js
+++ day 2/bag.js
@@ -1,4 +1,4 @@
-class bag {
+class Bag {
     //property 
     weight;
 
@@ -17,23 +17,13 @@ class bag {
         // this.weight > 23 ? true : false  Returning Undefined?
     } 
 }
-
-class plane {
-    //Property
-    passengers;
-  constructor (noOfPassengers){
-      this.passengers = noOfPassengers;
-  }
-
-
-}
-
+module.exports = Bag //Allows bag class to be used by other classes 
 
 
 
 //Test Code
-const hamzahBag = new bag(16)
-const bevBag = new bag(25)
+const hamzahBag = new Bag(16)
+const bevBag = new Bag(25)
 
 console.log(hamzahBag.isOverLimit())
 console.log(bevBag.isOverLimit())
--- day 2/passenger.js
+++ day 2/passenger.js
@@ -1,17 +1,28 @@
-class passenger {
+const Bag = require('./Bag') // Imports bag class 
+
+class Passenger {
     //Properties
   name;
   passportNum;
   seatNum;
+  bags;
 
 
 constructor(name, passportNum, seatNum){
     this.name = name;
     this.passportNum = passportNum;
     this.seatNum = seatNum
+    this.bags = [] //Empty Array that will hold the bag names and what they weigh
 }
 //Method
-addBag(){
-    
+addBag(bag){
+    this.bags.push(bag)
 }
+
 }
+const lisa = new Passenger('Lisa','234986', '23A');
+const lisaHandLuggage = new Bag(8);
+const lisaLargeLuggage = new Bag(22);
+lisa.addBag(lisaHandLuggage)
+lisa.addBag(lisaLargeLuggage)
+console.log(lisa.bags)
