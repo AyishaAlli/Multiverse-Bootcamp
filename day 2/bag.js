e2c1abc99c848b8b85b337b0494ed18b3e7b5835
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-05 12:09:51 +0000

    added passenger file

--- day 2/bag.js
+++ day 2/bag.js
@@ -27,24 +27,7 @@ class plane {
 
 }
 
-class passenger {
-    //Properties
-  name;
-  passportNum;
-  seatNum;
 
-constructor(name, passportNum, seatNum){
-    this.name = name;
-    this.passportNum = passportNum;
-    this.seatNum = seatNum
-}
-
-//Method
-addBag(){
-    
-}
-
-}
 
 
 //Test Code
---
+++ day 2/passenger.js
@@ -0,0 +1,16 @@
+class passenger {
+    //Properties
+  name;
+  passportNum;
+  seatNum;
+
+constructor(name, passportNum, seatNum){
+    this.name = name;
+    this.passportNum = passportNum;
+    this.seatNum = seatNum
+}
+//Method
+addBag(){
+    
+}
+}
