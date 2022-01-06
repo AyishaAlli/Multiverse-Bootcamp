b861a9a17da4e4875e5834e360b428d2ed4ae9e3
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-06 15:16:27 +0000

    added import line

--- Classes/Traveller.js
+++ Classes/Traveller.js
@@ -1,3 +1,4 @@
+const Bag = require("./Bag")
 
 class Traveller {
     name;
@@ -16,3 +17,14 @@ class Traveller {
 }
 
 module.exports = Traveller 
+
+const trav1 = new Traveller('helen')
+const trav2 = new Traveller('helen')
+const ff = new Bag(4)
+const cc = new Bag(8)
+trav1.addBag(ff)
+trav1.addBag(cc)
+//trav2.addBag(new Bag(5))
+//console.log(trav1.bags)
+console.log(trav1.bags)
+//console.log(Traveller)
