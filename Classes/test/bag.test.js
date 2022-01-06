0e662a9193fec2c0154106aa3646a7fff75efac5
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-06 14:13:33 +0000

    tested two files

---
+++ Classes/test/bag.test.js
@@ -0,0 +1,22 @@
+const Bag = require('../Bag')
+
+
+describe('defines a suite of tests for the Bag class', function () { 
+
+    test('has correct weight', function () {
+        // given
+        const mandysBag = new Bag(13);
+
+        // then
+        expect(mandysBag.weight).toBe(13);
+    });
+
+    test('cheks if over limit', function () {
+        // given
+        const mandysBag = new Bag(13);
+        
+        // then
+        expect(mandysBag.isOverLimit()).toBe(false);
+    });
+
+});
---
+++ Classes/test/passenger.test.js
@@ -0,0 +1,16 @@
+
+const Passenger = require('../Passenger')
+
+
+describe('defines a suite of tests for the Bag class', function () { 
+        test('returns object with information', function(){
+                //given
+                const lisa = new Passenger('Lisa','234986', '23A');
+
+                //then
+                expect(lisa).toBeDefined()
+                expect(lisa.name).toBe('Lisa')
+                expect(lisa.passportNum).toBe('234986')
+                expect(lisa.seatNum).toBe('23A')
+        })
+});
