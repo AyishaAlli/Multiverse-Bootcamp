fcd5ef253d5a3cf33685ffe328ecc6c335e73d2e
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-06 16:05:09 +0000

    added tests

---
+++ Classes/test/crewmember.test.js
@@ -0,0 +1,10 @@
+const Bag = require('./bag');
+const Traveller = require('./Traveller')
+
+
+describe('defines a suite of tests for the CrewMember class', function () { 
+
+    test('', function () {
+
+    });
+});
