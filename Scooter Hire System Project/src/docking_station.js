67a4e6ac699b3e97a4006421d7bf2eefbbfaacbd
Ayisha <alli.ayisha1@gmail.com>
2022-01-07 18:15:17 +0000

    added method

---
+++ Scooter Hire System Project/src/docking_station.js
@@ -0,0 +1,36 @@
+
+class DockingStation{
+    location
+    totalSpaces
+    scootersAvail
+    brokenScooters
+
+   constructor(location, totalSpaces, scootersAvail, brokenScooters){
+    this.location = location   
+    this.totalSpaces = totalSpaces;
+    this.scootersAvail = scootersAvail;
+    this.brokenScooters = brokenScooters;
+   }
+
+   areScootersAvail(){
+       //to check if you can get a scooter from this location
+       if (this.scootersAvail > 0){
+           return true
+       } else {
+           return false
+       }
+   }
+   isSpaceAvail(){
+       //to check if there's space to return the scooter 
+       if (this.totalSpaces - this.scootersAvail - this.brokenScooters >= 1){
+           return 'Yes theres space'
+       } else {
+           return false
+       }
+   }
+
+}
+
+const lewishamDockingStation = new DockingStation('Lewisham',10,5,0);
+console.log(lewishamDockingStation.areScootersAvail())
+console.log(lewishamDockingStation.isSpaceAvail())
