8357b45f9e3bf275f32249f72e2da5ca01552f60
Ayisha <alli.ayisha1@gmail.com>
2022-01-07 22:20:14 +0000

    added and ran test

---
+++ Scooter Hire System Project/test/scooter.test.js
@@ -0,0 +1,16 @@
+const { TestWatcher } = require("jest")
+const Scooter = require("../src/scooter");
+
+
+describe('testing', function(){
+    test('if working scooter is fully charge', function (){
+        //when
+        const newScooter = new Scooter('2312', 200, 'Working')
+        const anotherScooter = new Scooter('2312', 100, 'Working')
+
+        //then 
+        expect(newScooter.isFullyChargedandAvail()).toBe(true)
+        expect(anotherScooter.isFullyChargedandAvail()).toBe(false)
+        
+    }) 
+})
