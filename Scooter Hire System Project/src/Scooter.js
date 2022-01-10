36dd358421186a0f73027bb06c14d206d4e35a7e
Ayisha <alli.ayisha1@gmail.com>
2022-01-10 08:55:42 +0000

    added feature

--- Scooter Hire System Project/src/Scooter.js
+++ Scooter Hire System Project/src/Scooter.js
@@ -7,12 +7,14 @@ class Scooter {
     battery;
     condition;
     charging = false;
+    range;
     //location;
 
-    constructor(id, battery, condition, /*location*/){
+    constructor(id, battery, condition, range){
         this.id = id;
         this.battery = battery;
         this.condition = condition;
+        this.range = range
         //this.location = location
     }
     isFullyChargedandAvail(){
@@ -20,6 +22,17 @@ class Scooter {
           return true
         } else {
             return false
+        }
+    }
+
+    checkRange(scooter){
+        if (scooter.range = 32){
+            return 'Full Range'
+        } else if (scooter.range < 10){
+            return 'please start making your way to the nearest docking station'
+        }
+        else if (scooter.range < 3){
+            return 'please return your scooter to the nearest docking station'
         }
     }
     
--- Scooter Hire System Project/src/registered_customer.js
+++ Scooter Hire System Project/src/registered_customer.js
@@ -1,10 +1,12 @@
 const User = require('./User');
-const DockingStation = require('./docking_station')
+const DockingStation = require('./docking_station');
+const Scooter = require('./scooter');
 
 class RegisteredCustomer extends User{
     email;
     PaymentDetails = []
     withScooter = false;
+    scooterId;
     pastJourneys = [];
     
 
@@ -19,24 +21,33 @@ class RegisteredCustomer extends User{
 
         
     }
+    scanScooter(scooter){
+      if (scooter.condition != 'Working' && scooter.battery === 200){
+         //should allow 'start Journey' button to appear 
+      }
+    }
     
 
-    startJourney(scooter){ // how can i keep track of journeys?
-
+    startJourney(scooter){ 
         this.withScooter = true;
-        
-
+        this.scooterId = scooter.id
     
     }
 
     endJourney(scooter){
-        
         this.withScooter = false;
-
+        this.pastJourneys.push(this.scooterId)
+        this.scooterId = ''
     }
 }
 
 module.exports = RegisteredCustomer
 
-//const Rae = new RegisteredCustomer('Rae', 'aa@.com')
+const rae = new RegisteredCustomer('Rae', 'aa@.com')
+const scooter12 = new Scooter('1111', 200, 'Working', 32)
+
+rae.startJourney(scooter12)
+console.log(rae)
+rae.endJourney(scooter12)
+console.log(rae)
 
