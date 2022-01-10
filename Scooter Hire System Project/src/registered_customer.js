330e182c6a5c5b847a79aad6e2b6aa307c70806e
Ayisha <alli.ayisha1@gmail.com>
2022-01-10 17:35:43 +0000

    added date

--- Scooter Hire System Project/src/registered_customer.js
+++ Scooter Hire System Project/src/registered_customer.js
@@ -34,20 +34,22 @@ class RegisteredCustomer extends User{
     
     }
 
-    endJourney(scooter){
+    endJourney(){
+        const date = new Date()
         this.withScooter = false;
-        this.pastJourneys.push(this.scooterId)
+        this.pastJourneys.push(this.scooterId, date)
         this.scooterId = ''
+
     }
 }
 
 module.exports = RegisteredCustomer
 
-// const rae = new RegisteredCustomer('Rae', 'aa@.com')
-// const scooter12 = new Scooter('1111', 120, 'Working', 32)
+const rae = new RegisteredCustomer('Rae', 'aa@.com')
+const scooter12 = new Scooter('1111', 120, 'Working', 32)
 
-// rae.startJourney(scooter12)
-// console.log(rae)
-// rae.endJourney(scooter12)
-// console.log(rae)
+rae.startJourney(scooter12)
+//console.log(rae)
+rae.endJourney(scooter12)
+console.log(rae)
 
