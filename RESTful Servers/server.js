89b218a98882163615374c6d30530bd6d5bb664c
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-17 12:15:33 +0000

    created an end poiny

--- RESTful Servers/server.js
+++ RESTful Servers/server.js
@@ -6,6 +6,19 @@ const port = 3000; // Sets port to 3000
 app.use(express.static("public")); // Gets code from folder called public
 
 app.listen(port, () => {
+  // Creates End Point for flipping a coin 
+//   app.get("/flipcoin", (request, response) => {
+//     random = Math.floor(Math.random() * 2);
+//     if (random === 1) {
+//       response.send("Heads");
+//     } else {
+//       response.send("Tails");
+//     }
+//   });
+    // creates endpoint for restaurants
+    app.get("/restaurants", (request, response) => {
+          
+          });
   /*Listens out for that code and displays it on the webpage below */
   console.log(`Server listening at http://localhost:${port}`);
 });
