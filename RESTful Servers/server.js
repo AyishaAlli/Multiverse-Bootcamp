3fe8413d0f0a74101afc3af90d00e2ac6e941378
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-18 15:31:48 +0000

    added endpoints

--- RESTful Servers/server.js
+++ RESTful Servers/server.js
@@ -5,7 +5,11 @@ const app = express(); // Creates Server
 const port = 3000; // Sets port to 3000
 
 //const { Sequelize, DataTypes, Model } = require("sequelize");
-const { Restaurant } = require("./restaurant");
+const { Restaurant } = require("./Restaurant");
+
+//Middleware so that Express can read JSON and URL encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public")); // Gets code from folder called public
 
@@ -20,7 +24,7 @@ app.use(express.static("public")); // Gets code from folder called public
 //   }
 // });
 
-// creates endpoint for restaurants
+// creating endpoint for searching all restaurants
 app.get("/restaurants", async (req, res) => {
   /*const restaurant = await Restaurant.create({
     name: "Ronalds",
@@ -28,6 +32,34 @@ app.get("/restaurants", async (req, res) => {
   });*/
   const restaurants = await Restaurant.findAll();
   res.json(restaurants);
+});
+
+// new endpoint to search each restaurant by ID
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
+});
+
+//New endpoint to listen to CREATE a restaurant
+app.post("/restaurants", async (req, res) => {
+  const restaurant = await Restaurant.create(req.body);
+  res.sendStatus(201).json(restaurant);
+});
+
+//New endpoint to listen to UPDATE requests
+app.put("/restaurants/:id", async (req, res) => {
+  const data = await Restaurant.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.sendStatus(201).json(data);
+});
+
+//New endpoint to listen to DELETE requests
+app.delete("/restaurants/:id", async (req, res) => {
+  const data = await Restaurant.destroy({ where: { id: req.params.id } });
+  res.sendStatus(201).json(data);
 });
 
 app.listen(port, () => {
