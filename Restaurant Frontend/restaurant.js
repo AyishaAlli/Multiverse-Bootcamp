f94ff730669c394a3e59cadf4b795c9b63bbbf48
Ayisha Alli <alli.ayisha1@gmail.com>
2022-01-21 18:18:49 +0000

    restaurant styling

--- Restaurant Frontend/restaurant.js
+++ Restaurant Frontend/restaurant.js
@@ -1,7 +1,18 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelector(".menuItems");
+const openModal = document.querySelector(".fa-pencil-alt");
+const closeModal = document.querySelector(".fa-times");
+const form = document.querySelector(".editForm");
 
 menu.addEventListener("click", () => {
   menuItems.classList.toggle("hidden");
   console.log("works!:D");
+});
+
+openModal.addEventListener("click", () => {
+    console.log('hi')
+  form.style.display = "block";
+});
+closeModal.addEventListener("click", () => {
+  form.style.display = "none";
 });
